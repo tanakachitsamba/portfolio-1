6141246ce0b85c0a195595296380f6f5dcfec2be
Tanaka <alongthejourneyahead@gmail.com>
2023-08-09 00:36:04 +0100

    ...

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			// default options are shown
-			pages: 'local_',
-			assets: 'local_',
+			pages: 'local',
+			assets: 'local',
 			//fallback: '404.html',
 			precompress: false,
 			strict: false,
