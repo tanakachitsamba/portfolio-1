c78cb1cce3d034cea71a3c55f8dbdcb78c4a7aff
Tanaka <alongthejourneyahead@gmail.com>
2023-08-08 23:00:48 +0100

    updated codebase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 
+
 </script>
 
 <style lang="postcss">
 :global(html) {
-  background-color: theme(colors.white);
+  background-color: #efcaba;
   margin: 0; padding: 0; 
+  font-family: Poppins;
 }
 
 .container {
@@ -32,6 +34,21 @@
   grid-template-columns: 1fr; /* Set the number of columns as needed */
   grid-gap: 10px; /* Adjust the gap between divs as desired */
 }
+
+.c {
+  padding-top: 30px;
+}
+
+.title {
+  font-size: 20px;
+  line-height: 1.8;
+
+}
+
+.sub-heading {
+  font-size: 18px;
+  font-weight: 600;
+}
 </style>
 
 <main>
@@ -39,10 +56,11 @@
     <div class="nav_">🌑</div>
 
     <div class="about">
-      <div>Hey! I'm Tanaka. I am an aspiring data scientist and AI researcher, based in Nottingham, UK.</div>
+      <div class="title c">Hey! I'm Tanaka. I am an aspiring data scientist and AI researcher, based in Nottingham, UK. I'm a creative data enthusiast 📊🎨 with a knack for Python, JavaScript, and Golang. I transform data into insights, love building end-to-end data products ☁️, and experimenting with data modelling and machine learning to solve challenges. Passionate about ethical data practices and responsible machine learning use. 
+      </div>
       
-    <div>
-      Latest Work 
+    <div class="sub-heading c">
+      My work 
     </div>
     <div class="col">
       <div class="c">1</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-static-digitalocean';
+
+
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 
@@ -8,9 +12,20 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+            // default options are shown
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true,
+            spec: '.do/spec.yaml',
+            name: ''
+        }),
 	}, 
-	preprocess: vitePreprocess()
+	//preprocess: vitePreprocess()
 };
+
+
 
 export default config;
