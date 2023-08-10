6d4e7e4e25ebc2a34b209df94cb516266c9734c8
Tanaka <alongthejourneyahead@gmail.com>
2023-08-10 23:37:21 +0100

    updated for fly.to

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
+
 //import adapter from 'svelte-adapter-static-digitalocean';
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
