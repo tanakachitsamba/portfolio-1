07ee067875183934c9527c6f567e6f995cefde23
Tanaka <alongthejourneyahead@gmail.com>
2023-08-12 10:10:34 +0100

    updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,18 +7,22 @@
 		<div class="nav_">🌑</div>
 
 		<div class="about">
-
-			
 			<div class="title c">
 				Hey! I'm Tanaka. I am an aspiring data scientist and AI researcher, based in Nottingham, UK.
 				I'm a creative data enthusiast 📊🎨 with a passion for Python, JavaScript, and Golang. I
 				transform data into insights, love building end-to-end data products ☁️, and experimenting
-				with data modelling and machine learning to solve challenges. 
+				with data modelling and machine learning to solve challenges.
 			</div>
 
 			<div class="sub-heading c">My Work</div>
 			<div class="c e">The Gardening App</div>
-			<div class="d">I developed a prototype for an AI-powered gardening app, it harnesses the power of OpenAI's Language Model (LLM) API  to provide users with information about growing plants. The core objective of this  project revolves around the creation of an intelligent assistance platform that empowers gardeners with expert support and assistance across a wide range of growing tasks. The Data Science project report coming soon. </div>
+			<div class="d">
+				I developed a prototype for an AI-powered gardening app, it harnesses the power of OpenAI's
+				Language Model (LLM) API to provide users with information about growing plants. The core
+				objective of this project revolves around the creation of an intelligent assistance platform
+				that empowers gardeners with expert support and assistance across a wide range of growing
+				tasks. The Data Science project report coming soon.
+			</div>
 
 			<a href="http://165.22.120.128:5173/" class="text-link">Try out the prototype now 🌱</a>
 
@@ -87,21 +91,19 @@
 
 	/* Style the link to look like text with underline */
 	.text-link {
-	text-decoration: none; /* Remove default underline */
-	color: inherit; /* Use the default text color */
-	font-family: inherit; /* Use the default font */
-	font-size: inherit; /* Use the default font size */
-	transition: font-size 0.3s, text-decoration 0.3s; /* Add transitions for hover effect */
-	border-bottom: 1px solid currentColor; /* Add underline on hover using current text color */
+		text-decoration: none; /* Remove default underline */
+		color: inherit; /* Use the default text color */
+		font-family: inherit; /* Use the default font */
+		font-size: inherit; /* Use the default font size */
+		transition: font-size 0.3s, text-decoration 0.3s; /* Add transitions for hover effect */
+		border-bottom: 1px solid currentColor; /* Add underline on hover using current text color */
 	}
 
 	/* Add hover effect to increase text size and underline */
 	.text-link:hover {
-	font-size: 110%; /* Increase font size on hover */
-	border-bottom: 4px solid currentColor; /* Add underline on hover using current text color */
+		font-size: 110%; /* Increase font size on hover */
+		border-bottom: 4px solid currentColor; /* Add underline on hover using current text color */
 	}
-
-
 
 	.title {
 		font-size: 22px;
@@ -119,6 +121,7 @@
 		font-weight: 600;
 		font-style: italic;
 		color: rgb(76, 76, 76);
-		padding-top: 40px
+		padding-top: 40px;
+		padding-bottom: 20px;
 	}
 </style>
